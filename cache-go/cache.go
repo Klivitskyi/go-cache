59b950424797172675e4a17af3ebf111f1366359
Ivan Klivitskyi <vklivitskiy@gmail.com>
2024-01-28 10:49:18 +0100

    Added ttl for cache

--- cache-go/cache.go
+++ cache-go/cache.go
@@ -1,28 +1,68 @@
 package cacheGo
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type cacheItem struct {
+	value int
+	ttl   time.Time
+}
 
 type Cache struct {
-	make map[string]int
+	items map[string]cacheItem
+	mu    sync.Mutex
 }
 
-func New() Cache {
-	return Cache{make: make(map[string]int)}
+func New() *Cache {
+	return &Cache{items: make(map[string]cacheItem)}
 }
 
-func (c *Cache) Get(key string) int {
-	return c.make[key]
-}
+func (c *Cache) Get(key string) (int, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 
-func (c *Cache) Set(key string, value int) string {
-	if c.make == nil {
-		c.make = make(map[string]int)
+	item, found := c.items[key]
+
+	if !found || time.Now().After(item.ttl) {
+		return 0, false
 	}
 
-	c.make[key] = value
-	return fmt.Sprintf("Added %v with %v", key, value)
+	return item.value, true
+}
+
+func (c *Cache) Set(key string, value int, ttl time.Duration) string {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if c.items == nil {
+		c.items = make(map[string]cacheItem)
+	}
+
+	c.items[key] = cacheItem{
+		value: value,
+		ttl:   time.Now().Add(ttl),
+	}
+
+	return fmt.Sprintf("Added %v with %v, expires in %v\n", key, value, ttl)
 }
 
 func (c *Cache) Delete(key string) {
-	delete(c.make, key)
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	delete(c.items, key)
+}
+
+func (c *Cache) CleanUp() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	for key, item := range c.items {
+		if time.Now().After(item.ttl) {
+			delete(c.items, key)
+		}
+	}
 }
--- main.go
+++ main.go
@@ -3,18 +3,27 @@ package main
 import (
 	cacheGo "Klivitskyi/go-cache/cache-go"
 	"fmt"
+	"time"
 )
 
 func main() {
 	cache := cacheGo.New()
 
-	fmt.Println(cache.Set("userId", 42))
-	userId := cache.Get("userId")
+	fmt.Print(cache.Set("userId", 42, time.Second))
+
+	time.Sleep(time.Second * 2)
+
+	userId, ok := cache.Get("userId")
+
+	if !ok {
+		fmt.Printf("The user with ID %v didn't find\n", userId)
+		return
+	}
 
 	fmt.Println(userId)
 
 	cache.Delete("userId")
-	userId = cache.Get("userId")
+	userId, ok = cache.Get("userId")
 
-	fmt.Println(userId)
+	fmt.Println(userId, ok)
 }
